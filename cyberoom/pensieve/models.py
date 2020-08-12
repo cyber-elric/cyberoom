6ddf941759a017a445ddd1c27f6b2370ff840725
cyberelric <elricleung@126.com>
2020-08-12 18:29:29 +0800

    将pensieve显示的时间从发布时间改为发布时间或上次修改时间

--- cyberoom/pensieve/models.py
+++ cyberoom/pensieve/models.py
@@ -8,7 +8,7 @@ from tinymce.models import HTMLField
 class Pensieve(models.Model):
     title = models.CharField(unique=True, max_length=66)
     content = HTMLField(blank=True)
-    upDate = models.DateTimeField(auto_now_add=True)
+    upDate = models.DateTimeField(auto_now=True)
     up = models.CharField(max_length=60)
 
     def __str__(self):
